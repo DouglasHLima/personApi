1def72fb3d5ad4ed1a5af611526a5d5ab2af8c86
DouglasHLima <douglashdelima@gmail.com>
2022-02-01 19:30:46 -0300

    feat: developed list all method

--- src/main/java/com/doughlima/personapi/service/PersonService.java
+++ src/main/java/com/doughlima/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import com.doughlima.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class PersonService {
@@ -20,12 +23,20 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(PersonDTO personDTO){
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
