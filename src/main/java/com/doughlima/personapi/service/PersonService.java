33074f83c84ce63466707911b94b653b899cdaa3
DouglasHLima <douglashdelima@gmail.com>
2022-01-31 23:50:08 -0300

    feat: improved person creation functionality

--- src/main/java/com/doughlima/personapi/service/PersonService.java
+++ src/main/java/com/doughlima/personapi/service/PersonService.java
@@ -1,7 +1,9 @@
 package com.doughlima.personapi.service;
 
+import com.doughlima.personapi.dto.request.PersonDTO;
 import com.doughlima.personapi.dto.response.MessageResponseDTO;
 import com.doughlima.personapi.entity.Person;
+import com.doughlima.personapi.mapper.PersonMapper;
 import com.doughlima.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PersonService {
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
     private final PersonRepository personRepository;
 
     @Autowired
@@ -17,8 +20,9 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
